7f43a69497b7f455d80c64ffac35dd085a10e96c
anironL <viasolaris@gmail.com>
2022-11-28 18:02:41 -0500

    EJS Update
    EJS template added html skeleton for testing/setup purposes

--- express_server.js
+++ express_server.js
@@ -13,10 +13,21 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
+/*
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -29,6 +40,7 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+*/
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
