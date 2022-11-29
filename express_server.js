caa6e2356bbce21dbd1cc618b27c84e921ea10ca
anironL <viasolaris@gmail.com>
2022-11-28 21:00:12 -0500

    new ID POST update
    new route test successful

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,43 +11,31 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+const users = {};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  const templateVars = { urls: urlDatabase }
+  res.render("urls_new", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.params); // Log the POST request body to the console
+  res.send(req.body.longURL); // Respond with 'Ok' (we will replace this)
+//  res.send("OK");
+});
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: href="#" };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] } //req.body.newURL }; //href="#" };
   res.render("urls_show", templateVars);
 });
-
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
-
-/*
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
-*/
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
